e396bea85a0a5850ef2fa39a52d6eaba12a83b4f
Vidrep <Vidrep@telus.net>
2017-09-09 14:43:50 -0600

    Add CompilationDVDView.cpp and CompilationDVDView.h

---
+++ Source/CompilationDVDView.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2010-2016, BurnItNow Team. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _COMPILATIONDVDVIEW_H_
+#define _COMPILATIONDVDVIEW_H_
+
+
+#include "BurnWindow.h"
+#include "PathView.h"
+
+#include <Button.h>
+#include <FilePanel.h>
+#include <Menu.h>
+#include <SeparatorView.h>
+#include <TextView.h>
+#include <View.h>
+
+
+class CommandThread;
+
+
+class DVDRefFilter : public BRefFilter {
+public:
+					DVDRefFilter() {};
+	virtual			~DVDRefFilter() {};
+
+	bool			Filter(const entry_ref* ref, BNode* node,
+						struct stat_beos* st, const char* filetype);
+};
+
+
+class CompilationDVDView : public BView {
+public:
+					CompilationDVDView(BurnWindow& parent);
+	virtual 		~CompilationDVDView();
+
+	virtual void	MessageReceived(BMessage* message);
+	virtual void	AttachedToWindow();
+	
+	void			BuildISO();
+	void			BurnDisc();
+
+private:
+	void 			_ChooseDirectory();
+	void 			_FromScratch();
+	void 			_OpenDirectory(BMessage* message);
+	void 			_BurnerOutput(BMessage* message);
+
+	BFilePanel* 	fOpenPanel;
+	CommandThread* 	fBurnerThread;
+	BTextView* 		fBurnerInfoTextView;
+	BurnWindow* 	windowParent;
+	BSeparatorView*	fBurnerInfoBox;
+	PathView*		fPathView;
+	BButton*		fVideoButton;
+	BButton*		fAudioButton;
+	BButton*		fImageButton;
+	BButton*		fBurnButton;
+
+	BPath* 			fDirPath;
+	BPath* 			fImagePath;
+	int				step;
+
+};
+
+
+#endif	// _COMPILATIONDVDVIEW_H_
